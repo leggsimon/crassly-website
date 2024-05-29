638c3a03a9cbaeaa020a9fef8b4321f07d1b9212
Simon Legg <leggsimon@gmail.com>
2024-05-29 15:09:44 +0200

    add tailwind color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,17 @@ export default {
 		fontFamily: {
 			display: ['Rowdies', 'sans-serif'],
 		},
+		colors: {
+			transparent: 'transparent',
+			white: '#ffffff',
+			black: '#000000',
+			linen: '#F8F7F3',
+			orange: '#EF7850',
+			casal: '#2A5462',
+			lavender: '#ABA9FC',
+			sand: '#FCE1AA',
+			peach: '#FEE3C6',
+		},
 		extend: {},
 	},
 	plugins: [],
